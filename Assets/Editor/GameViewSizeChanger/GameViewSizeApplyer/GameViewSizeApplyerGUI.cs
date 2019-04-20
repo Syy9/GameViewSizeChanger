5f493ac6965b51c4114c6ad110272d1d1aa35fca
Syy <shirakawa.yy@gmail.com>
2019-04-20 18:56:45 +0900

    Code clean

--- Assets/Editor/GameViewSizeChanger/GameViewSizeApplyer/GameViewSizeApplyerGUI.cs
+++ Assets/Editor/GameViewSizeChanger/GameViewSizeApplyer/GameViewSizeApplyerGUI.cs
@@ -6,6 +6,8 @@ namespace Syy.Tools
     public class GameViewSizeApplyerGUI
     {
         IGameViewSizeData _data;
+        GUIStyle _style;
+
         public GameViewSizeApplyerGUI(IGameViewSizeData data)
         {
             _data = data;
@@ -13,15 +15,19 @@ namespace Syy.Tools
 
         public bool OnGUI(bool isHighlight)
         {
+            if (_style == null)
+            {
+                _style = new GUIStyle(GUI.skin.box);
+                _style.normal.textColor = EditorStyles.toolbar.normal.textColor;
+            }
+
             var defaultColor = GUI.color;
             if (isHighlight)
             {
                 GUI.color = Color.gray;
             }
 
-            var style = new GUIStyle(GUI.skin.box);
-            style.normal.textColor = EditorStyles.toolbar.normal.textColor;
-            if (GUILayout.Button(_data.ToText(), style, GUILayout.ExpandWidth(true)))
+            if (GUILayout.Button(_data.ToText(), _style, GUILayout.ExpandWidth(true)))
             {
                 GUI.color = defaultColor;
                 return true;
